455d6fb4396b605e9e5360626adadccf28602803
sumin <115655543+cleonno3o@users.noreply.github.com>
2024-10-05 22:40:54 +0900

    [Gold IV] Title: 이분 그래프, Time: 708 ms, Memory: 5744 KB -BaekjoonHub

---
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	int k;
+	cin >> k;
+	for (int i = 0; i < k; i++)
+	{
+		int v, e;
+		cin >> v >> e;
+		vector<vector<int>> gr(v + 1);
+		vector<int> ch(v + 1);
+		for (int j = 0; j < e; j++)
+		{
+			int a, b;
+			cin >> a >> b;
+			gr[a].push_back(b);
+			gr[b].push_back(a);
+		}
+		bool flag = true;
+		for (int k = 1; k <= v; k++)
+		{
+			queue<int> q;
+			if (ch[k] == 0 && flag)
+			{
+				ch[k] = 1;
+				q.push(k);
+				while (!q.empty() && flag)
+				{
+					int s = q.front();
+					q.pop();
+					for (int j = 0; j < gr[s].size(); j++)
+					{
+						if (!flag) break;
+						if (ch[gr[s][j]] == 0)
+						{
+							q.push(gr[s][j]);
+							ch[gr[s][j]] = -ch[s];
+						}
+						else if (ch[gr[s][j]] == ch[s])
+						{
+							flag = false;
+							break;
+						}
+					}
+				}
+			}
+		}
+		if (flag) cout << "YES\n";
+		else cout << "NO\n";
+ 	}
+}
