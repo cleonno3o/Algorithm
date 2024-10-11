e3e30f9410e81e5f5e78d523c915a052da2cd61b
sumin <115655543+cleonno3o@users.noreply.github.com>
2024-10-11 12:40:56 +0900

    [Silver I] Title: 봄버맨, Time: 44 ms, Memory: 2176 KB -BaekjoonHub

---
+++ 백준/Silver/16918. 봄버맨/봄버맨.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+int board[200][200];
+int R, C, N;
+int d_row[] = { 0, 1, 0, -1 };
+int d_col[] = { 1, 0, -1, 0 };
+int main()
+{
+	cin >> R >> C >> N;
+	for (int i = 0; i < R; i++)
+	{
+		for (int j = 0; j < C; j++)
+		{
+			char c;
+			cin >> c;
+			if (c == 'O') board[i][j] = -1;
+		}
+	}
+	for (int turn = 1; turn <= N; turn++)
+	{
+		if (turn == 1) continue;
+		else if (turn % 2 == 0)
+		{
+			for (int i = 0; i < R; i++)
+			{
+				for (int j = 0; j < C; j++)
+				{
+					if (board[i][j] == 0) board[i][j] = turn;
+				}
+			}
+		}
+		else if (turn % 2 == 1)
+		{
+			for (int i = 0; i < R; i++)
+			{
+				for (int j = 0; j < C; j++)
+				{
+					if ((board[i][j] != 0 and board[i][j] == turn - 3) or board[i][j] == -1)
+					{
+						board[i][j] = 0;
+						for (int k = 0; k < 4; k++)
+						{
+							int n_row = i + d_row[k];
+							int n_col = j + d_col[k];
+							if (n_row >= R or n_row < 0 or n_col >= C or n_col < 0) continue;
+							if ((board[n_row][n_col] != 0 and board[n_row][n_col] == turn - 3) || board[n_row][n_col] == -1) continue;
+							board[n_row][n_col] = 0;
+						}
+					}
+				}
+			}
+		}
+	}
+	for (int i = 0; i < R; i++)
+	{
+		for (int j = 0; j < C; j++)
+		{
+			if (board[i][j] == 0) cout << ".";
+			else cout << "O";
+		}
+		cout << "\n";
+	}
+}
