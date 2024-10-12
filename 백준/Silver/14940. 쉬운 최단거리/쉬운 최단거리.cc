d686f0358174cd94b07ba32cfd3e7bff7011970d
sumin <115655543+cleonno3o@users.noreply.github.com>
2024-10-12 16:00:39 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 72 ms, Memory: 4004 KB -BaekjoonHub

---
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+vector<vector<int> > board;
+vector<vector<int> > ch;
+int drow[] = { -1, 1, 0, 0 };
+int dcol[] = { 0, 0, -1, 1 };
+pair<int, int> start;
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	board = vector<vector<int> >(n, vector<int>(m));
+	ch = vector<vector<int> >(n, vector<int>(m, -1));
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cin >> board[i][j];
+			if (board[i][j] == 2)
+				start = make_pair(i, j);
+		}
+	}
+	queue<pair<int, int> > q;
+	q.push(make_pair(start.first , start.second));
+	ch[start.first][start.second] = 0;
+	while (!q.empty())
+	{
+		pair<int, int> pos = q.front();
+		q.pop();
+		for (int i = 0; i < 4; i++)
+		{
+			int nRow = pos.first + drow[i];
+			int nCol = pos.second + dcol[i];
+			if (nRow >= n or nRow < 0 or nCol >= m or nCol < 0)
+				continue;
+			if (ch[nRow][nCol] != -1) continue;
+			if (board[nRow][nCol] == 0) continue;
+			ch[nRow][nCol] = ch[pos.first][pos.second] + 1;
+			q.push(make_pair(nRow, nCol));
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (board[i][j] == 0) cout << 0 << " ";
+			else cout << ch[i][j] << ' ';
+		}
+		cout << "\n";
+	}
+}
