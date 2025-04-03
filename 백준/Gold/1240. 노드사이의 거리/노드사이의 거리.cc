34399be0ba74ac5894b93c679d9d179bdef0ceb3
sumin <115655543+cleonno3o@users.noreply.github.com>
2025-04-03 19:27:27 +0900

    [Gold V] Title: 노드사이의 거리, Time: 688 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
@@ -5,38 +5,39 @@ using namespace std;
 
 int N, M;
 vector<vector<int>> graph;
+vector<vector<int>> dist;
 void dijkstra(int s, int e)
 {
     int res = 2147000000;
-    vector<int> dist(N + 1, 2147000000);
     priority_queue<pair<int, int>> pq;
     pq.push({0, s});
-    dist[s] = 0;
+    dist[s][s] = 0;
     while (!pq.empty())
     {
         int cost = pq.top().first;
         int pos = pq.top().second;
         pq.pop();
-        if (-cost > dist[pos]) continue;
+        if (-cost > dist[s][pos]) continue;
         for (int i = 1; i <= N; i++)
         {
             if (graph[pos][i] != -1)
             {
-                if (dist[i] > -cost + graph[pos][i])
+                if (dist[s][i] > -cost + graph[pos][i])
                 {
-                    dist[i] = -cost + graph[pos][i];
-                    pq.push({-dist[i], i});
+                    dist[s][i] = -cost + graph[pos][i];
+                    pq.push({-dist[s][i], i});
                 }
             }
         }
     }
-    cout << dist[e] << "\n";
+    cout << dist[s][e] << "\n";
 }
 
 int main()
 {
     cin >> N >> M;
     graph = vector<vector<int>>(N + 1, vector<int>(N + 1, -1));
+    dist = vector<vector<int>>(N + 1, vector<int>(N + 1, 2147000000));
     for (int i = 1; i <= N - 1; i++)
     {
         int s, e, dist;
