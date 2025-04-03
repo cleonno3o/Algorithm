9949e4a5593112bb6fb3c712e4cfb8ecb8bbfa4a
sumin <115655543+cleonno3o@users.noreply.github.com>
2025-04-03 19:32:55 +0900

    [Gold V] Title: 노드사이의 거리, Time: 28 ms, Memory: 2092 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.cc
@@ -1,55 +1,67 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 using namespace std;
+const int MAX = 1001;
+const int LOG = 10; // log2(1000) ≈ 10
 
-int N, M;
-vector<vector<int>> graph;
-vector<vector<int>> dist;
-void dijkstra(int s, int e)
-{
-    int res = 2147000000;
-    priority_queue<pair<int, int>> pq;
-    pq.push({0, s});
-    dist[s][s] = 0;
-    while (!pq.empty())
-    {
-        int cost = pq.top().first;
-        int pos = pq.top().second;
-        pq.pop();
-        if (-cost > dist[s][pos]) continue;
-        for (int i = 1; i <= N; i++)
-        {
-            if (graph[pos][i] != -1)
-            {
-                if (dist[s][i] > -cost + graph[pos][i])
-                {
-                    dist[s][i] = -cost + graph[pos][i];
-                    pq.push({-dist[s][i], i});
-                }
-            }
-        }
+vector<pair<int, int>> tree[MAX];
+int parent[MAX][LOG]; // 2^k번째 조상
+int depth[MAX];
+int dist[MAX];
+
+void dfs(int curr, int par, int d) {
+    depth[curr] = depth[par] + 1;
+    parent[curr][0] = par;
+    dist[curr] = d;
+
+    for (auto [next, w] : tree[curr]) {
+        if (next != par) dfs(next, curr, d + w);
     }
-    cout << dist[s][e] << "\n";
 }
 
-int main()
-{
+void buildLCA(int N) {
+    for (int k = 1; k < LOG; k++) {
+        for (int v = 1; v <= N; v++) {
+            parent[v][k] = parent[parent[v][k - 1]][k - 1];
+        }
+    }
+}
+
+int lca(int u, int v) {
+    if (depth[u] < depth[v]) swap(u, v);
+    for (int k = LOG - 1; k >= 0; k--) {
+        if (depth[u] - (1 << k) >= depth[v]) {
+            u = parent[u][k];
+        }
+    }
+    if (u == v) return u;
+    for (int k = LOG - 1; k >= 0; k--) {
+        if (parent[u][k] != parent[v][k]) {
+            u = parent[u][k];
+            v = parent[v][k];
+        }
+    }
+    return parent[u][0];
+}
+
+int main() {
+    int N, M;
     cin >> N >> M;
-    graph = vector<vector<int>>(N + 1, vector<int>(N + 1, -1));
-    dist = vector<vector<int>>(N + 1, vector<int>(N + 1, 2147000000));
-    for (int i = 1; i <= N - 1; i++)
-    {
-        int s, e, dist;
-        cin >> s >> e >> dist;
-        graph[s][e] = dist;
-        graph[e][s] = dist;
+
+    for (int i = 1; i <= N - 1; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        tree[u].push_back({v, w});
+        tree[v].push_back({u, w});
     }
-    for (int i = 1; i <= M; i++)
-    {
-        int s, e;
-        cin >> s >> e;
-        dijkstra(s, e);
+
+    dfs(1, 0, 0);     // 루트는 1
+    buildLCA(N);      // LCA 전처리
+
+    for (int i = 0; i < M; i++) {
+        int u, v;
+        cin >> u >> v;
+        int anc = lca(u, v);
+        cout << dist[u] + dist[v] - 2 * dist[anc] << '\n';
     }
-    
 }
